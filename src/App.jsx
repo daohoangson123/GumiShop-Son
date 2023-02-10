c43bba139c322db3645d8d28c8df61f7496b4bef
daohoangson123 <108396570+daohoangson123@users.noreply.github.com>
2023-02-10 22:16:05 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App '>
+      <header>
+        <div>SignBar</div>
+        <nav></nav>
+        <div>Hero</div>
+      </header>
+      <main>
+        <section>GOOD$ME DEAL</section>
+        <section>TAKE CONTROL</section>
+        <section>OUR PRODUCTS</section>
+        <section>CAROUSEL</section>
+        <section>SHOP ALL</section>
+        <section>LATEST NEWS</section>
+        <section>#GOOD4ME</section>
+      </main>
+      <footer>
+        <div>GOOD4ME</div>
+        <div>KEEP IN TOUCH</div>
+        <div>MORE INFO</div>
+        <div>COPYRIGHT</div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
